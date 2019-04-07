50962f9b58bb32024fd949a67bf76357a6f577f3
ismglv <ismglvb@yandex.ru>
2019-04-07 22:47:17 +0300

    fix

--- hw2.py
+++ hw2.py
@@ -1,66 +1,44 @@
-import psycopg2
-import uuid
-
-# предполагается установленный модуль nltk
+from pymystem3 import Mystem
+from nltk.stem.snowball import SnowballStemmer
 import nltk
-
-# скачиваем нужные компоненты, при вызове этой команды откроется gui интерфейс, нужно выбрать popular (либо all) 
-nltk.download()
-
-from nltk.corpus import stopwords
-
-# стеммер Портера предназначен только для английских слов, Snowball стеммер является развитием стеммера Портера
-from nltk.stem.snowball import SnowballStemmer 
-
+import json
+from IPython import embed
+from db import DB
 import re
 import string
+import uuid
 
-# обертка для MyStem
-from pymystem3 import Mystem
+m = Mystem()
+stemmer_rus = SnowballStemmer('russian')
 
-conn = psycopg2.connect(database="search_1", user="vlados", password="123456", host="localhost", port="5433")
-cur = conn.cursor()
+db = DB()
 
-cur.execute("CREATE TABLE IF NOT EXISTS words_porter(id uuid PRIMARY KEY, term varchar(64), article_id uuid)")
-cur.execute("CREATE TABLE IF NOT EXISTS words_mystem(id uuid PRIMARY KEY, term varchar(64), article_id uuid)")
+articles = db.get_articles()
 
-cur.execute("SELECT * FROM articles;")
+puncList = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
 
-words_porter = []
-words_mystem = []
+filename = "/Users/ismglv/dev/info-search-homeworks/stop-words.txt"
+with open(filename, 'r') as f:
+    stopwords = [line.strip() for line in f]   # save numbers to a list
 
-for item in cur:
-    text = item[1] + " " + item[2] + " " + item[3]
+words = []
+for (id, article) in articles:
+    article = ''.join(article).translate(string.punctuation)
+    for punc in puncList:
+        for word in article:
+            article = article.replace(punc,'')
+    words = article.split()
 
-    words = re.split(r'\W+', text)
-    words = [w.lower() for w in words if w.isalpha()]
+    my_stem = []
+    for word in words:
+        if(word not in stopwords):
+            t = m.lemmatize(word)[0]
+            my_stem.append(t)
+            db.insert_my_stem(str(uuid.uuid4()), t, id)
 
-    stop_words = set(stopwords.words('russian'))
-    words = [w for w in words if not w in stop_words]
-
-    stemmer_eng = SnowballStemmer('english')
-    stemmer_rus = SnowballStemmer('russian')
-    stemmed = [stemmer_rus.stem(stemmer_eng.stem(word)) for word in words]
-
-    for w in stemmed:
-        words_porter.append((str(uuid.uuid4()), w, item[0]))
-    
-    # саму программу я добавил в gitignore из-за большого веса, обертка может сама скачивать MyStem, если не указывать путь к бинарнику
-    stemmer_ya = Mystem(mystem_bin='./mystem')
-    lemmas = stemmer_ya.lemmatize(' '.join(words))
-    lemmas = [l for l in lemmas if len(l.strip()) > 0]
-
-    for w in lemmas:
-        words_mystem.append((str(uuid.uuid4()), w, item[0]))
-
-    conn.commit()
-
-for w in words_porter:
-    cur.execute("INSERT INTO words_porter VALUES(%s, %s, %s)", w)
-
-for w in words_mystem:
-    cur.execute("INSERT INTO words_mystem VALUES(%s, %s, %s)", w)
-
-conn.commit()
-cur.close()
-conn.close()
+    porter = []
+    for word in words:
+        if(word not in stopwords):
+            t = stemmer_rus.stem(word)
+            porter.append(t)
+            db.insert_porter(str(uuid.uuid4()), t, id)
